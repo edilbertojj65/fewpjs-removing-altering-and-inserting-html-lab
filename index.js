107ecbf7c658cdfaf652b7d5329380164982c3f1
edilbertojj65 <edilbertojj65@gmail.com>
2021-04-06 17:13:10 -0400

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove('main');
+const newHeader = document.createElement(`h1`);
+document.body.appendChild(newHeader);
+let h1 = document.getElementById("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Edilberto is the champion>" 
+newHeader.className = "victory";
+
+
